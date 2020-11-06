c087d29e6c99329e244e46dc8916824006147d28
reinis <reinis@foremdigital.eu>
2020-11-06 18:03:57 +0200

    Add POST

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(40),
+            'slug' => Str::of($this->faker->text(10))->slug('-'),
+            'content' => $this->faker->paragraph(10, true),
+
+        ];
+    }
+}
